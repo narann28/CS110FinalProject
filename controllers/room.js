eedd59ad91e14693bd3d4d47cc5b3535026516c1
Naran Nathan <nnath003@ucr.edu>
2024-06-06 16:28:00 -0700

    edit and delete messages

--- controllers/room.js
+++ controllers/room.js
@@ -49,4 +49,31 @@ const postMessage = async (req, res) => {
   }
 };
 
-module.exports = { createRoom, getRoom, getMessages, postMessage };
+const editMessage = async (req, res) => {
+  try {
+    const { messageId } = req.params;
+    const { text } = req.body;
+    const updatedMessage = await Message.findByIdAndUpdate(messageId, { text, edited: true }, { new: true });
+    if (!updatedMessage) {
+      return res.status(404).send('Message not found');
+    }
+    res.json(updatedMessage);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+const deleteMessage = async (req, res) => {
+  try {
+    const { messageId } = req.params;
+    const deletedMessage = await Message.findByIdAndDelete(messageId);
+    if (!deletedMessage) {
+      return res.status(404).send('Message not found');
+    }
+    res.json({ message: 'Message deleted successfully' });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+module.exports = { createRoom, getRoom, getMessages, postMessage, editMessage, deleteMessage };
--- models/Message.js
+++ models/Message.js
@@ -4,7 +4,8 @@ const messageSchema = new mongoose.Schema({
   roomId: String,
   nickname: String,
   text: String,
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now },
+  edited: { type: Boolean, default: false }
 });
 
 module.exports = mongoose.model('Message', messageSchema);
